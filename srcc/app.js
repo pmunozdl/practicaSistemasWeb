acf22f8fa840f7b34c3911aa56c23f0aabfedc58
Pablo <p.munoz28@usp.ceu.es>
2023-12-05 17:14:27 +0100

    modifico index

--- srcc/app.js
+++ srcc/app.js
@@ -5,8 +5,9 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const inicioRouter = require('./routes/inicio');
 const usersRouter = require('./routes/users');
+const sobreNosotrosRouter = require('./routes/sobreNosotros');
+
 
 const app = express();
 
@@ -21,8 +22,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/inicio', inicioRouter);
 app.use('/users', usersRouter);
+app.use('/sobreNosotros', sobreNosotrosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
