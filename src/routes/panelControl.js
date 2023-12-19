ed749c56a952c11a7445db8a5d8296d357bc174e
Pablo Muñoz de lorenzo <pablomunozdelorenzo@MBP-de-Pablo.home>
2023-12-20 00:21:59 +0100

    añado método borrado

--- src/routes/panelControl.js
+++ src/routes/panelControl.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const { Sequelize, Model, where } = require('sequelize'); //cargo la librería sequelize
+const { models } = require('../sequelize');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('panelControl', { title: 'Panel de Control Administrador', user: req.session.user });
+router.get('/', async function(req, res, next) {
+  let usuarios = await models.user.findAll({where: {rol:"user"}});
+  res.render('panelControl', { title: 'Panel de Control Administrador',usuarios,user: req.session.user });
 });
 
+router.post('/deleteUser',  async function(req, res, next){ // método para borrar usuarios. Puede haber varios post en una clase
+  const username = req.body.username;
+  if(req.session.user.rol == "admin") { // si el rol de usuario es igual a admin, puede acceder a la pestaña. 
+      console.log(usuario);
+      await models.user.destroy({
+        where: {
+          username: username
+        },
+      });; // destroy para sequelize. En arrays basta con con database.users.data[username]; 
+      res.redirect("/interfazAdmin");
+  }else{
+      req.session.error = "Unauthorized access"; // si no es admin, no puede.
+      res.redirect("/");
+  }
+});
 module.exports = router;
