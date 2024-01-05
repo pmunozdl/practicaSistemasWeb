57452715f86f8d0b311b96a3775e334c91e31a1a
Pablo Muñoz de lorenzo <pablomunozdelorenzo@MBP-de-Pablo.home>
2024-01-06 00:58:56 +0100

    empiezo a añadir mensajes en transferencia y bizum

--- src/routes/bizum.js
+++ src/routes/bizum.js
@@ -17,31 +17,34 @@ router.post('/', async function(req, res, next){
   let username = req.session.user.username;//emisor
   let receptor2 = await models.user.findOne({where: { username : receptor}});
   let user = await models.user.findOne({where: {username: username}});
-  let saldo2 = receptor2.dataValues.saldo; //saldo receptor
   let saldo = user.dataValues.saldo; //saldo emisor
   //const user = await models.user.findOne({ where: { username } });  //agarrar name de input
   
   
   
   if (receptor2) { //si existe el receptor sigue. Si no, cancela operación
-    if(saldo >= cantidad) {
-      if (!isNaN(cantidad) && cantidad <= 50) {
+    let saldo2 = receptor2.dataValues.saldo; //saldo receptor
+    if (!isNaN(cantidad) && cantidad <= 50) {
+      if(saldo >= cantidad) {
         const nuevaTransacion = await newTransaccion(username, receptor, cantidad);
         user.update({saldo : saldo - cantidad}, {where: { user: user}});
         receptor2.update({saldo: saldo2 + cantidad}, {where: {username: receptor}});
-        let mensaje = "Operación realizada con éxito"
+        let mensaje = "Operación realizada con éxito";
         res.render('bizum', { title: 'Bizum',  user: req.session.user, mensaje});
       } else {
-        req.session.error = "El valor introducido no es un número";
-        res.redirect("/bizum");
+        req.session.error="No tiene suficiente dinero para realizar la operación.";
+        let mensaje = "No tienes suficiente dinero";
+        res.render('bizum', { title: 'Bizum',  user: req.session.user, mensaje});
       } 
     }else {
-      req.session.error="No tiene suficiente dinero para realizar la operación.";
-      res.redirect("/bizum");
+      req.session.error = "El valor introducido no es un número";
+      let mensaje = "El valor introducido no es un número, o supera los 50$";
+      res.render('bizum', { title: 'Bizum',  user: req.session.user, mensaje});
     } 
-  } else {
+  } else if(!receptor2){
     req.session.error = "No existe ese nombre";
-    res.redirect("/bizum");
+    let mensaje = "El usuario introducido no existe";
+    res.render('bizum', { title: 'Bizum',  user: req.session.user, mensaje});
   }
 });
 
--- src/routes/transferencia.js
+++ src/routes/transferencia.js
@@ -15,31 +15,34 @@ router.post('/', async function(req, res, next){
   let username = req.session.user.username;//emisor
   let receptor2 = await models.user.findOne({where: { username : receptor}});
   let user = await models.user.findOne({where: {username: username}});
-  let saldo2 = receptor2.dataValues.saldo; //saldo receptor
   let saldo = user.dataValues.saldo; //saldo emisor
   //const user = await models.user.findOne({ where: { username } });  //agarrar name de input
   
   
   
   if (receptor2) { //si existe el receptor sigue. Si no, cancela operación
-    if(saldo >= cantidad) {
-      if (!isNaN(cantidad)) {
+    let saldo2 = receptor2.dataValues.saldo; //saldo receptor
+    if (!isNaN(cantidad)) {
+      if(saldo >= cantidad) {
         const nuevaTransacion = await newTransaccion(username, receptor, cantidad);
         user.update({saldo : saldo - cantidad}, {where: { user: user}});
         let mensaje = "Operación realizada con éxito"
         res.render('transferencia', { title: 'Transferencia',  user: req.session.user, mensaje});
         // receptor2.update({saldo: saldo2 + cantidad}, {where: {username: receptor}});
       } else {
-        req.session.error = "El valor introducido no es un número";
-        res.redirect("/transferencia");
+        req.session.error="No tiene suficiente dinero para realizar la operación.";
+        let mensaje = "No tiene suficiente dinero para realizar la operación."
+        res.render('transferencia', { title: 'Transferencia',  user: req.session.user, mensaje});
       } 
     }else {
-      req.session.error="No tiene suficiente dinero para realizar la operación.";
-      res.redirect("/bizum");
+      req.session.error = "La cantidad introducida no es válida.";
+      let mensaje = "La cantidad introducida no es válida."
+      res.render('transferencia', { title: 'Transferencia',  user: req.session.user, mensaje});
     } 
   } else {
-    req.session.error = "No existe ese nombre";
-    res.redirect("/inicioUsuarioRegistrado");
+    req.session.error = "No existe ese usuario";
+    let mensaje = "No existe ese usuario"
+    res.render('transferencia', { title: 'Transferencia',  user: req.session.user, mensaje});
   }
 });
 
